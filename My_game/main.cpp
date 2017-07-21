0d0cfdbc8e7f52df7754717b57df74f2727fba9e
maurizio <n.i.k.tver@yandex.ru>
2017-07-22 00:40:08 +0300

    First game framework

--- My_game/main.cpp
+++ My_game/main.cpp
@@ -1,36 +1,17 @@
 #include <iostream>
-#include "SFML/Graphics.hpp"
-
-using namespace std;
-using namespace sf;
+#include "game.h"
 
 int main()
 {
-    //Initialize a window
-    RenderWindow window (VideoMode(640,480),"First_lesson");
+    //Entry point
+    Game game;
 
-    // Creating playerSprite
-    Texture playerTexture;
-    playerTexture.loadFromFile("Media/Images/Mushroom.png");
-    Vector2u size = playerTexture.getSize();
-    Sprite player (playerTexture);
-    player.setOrigin(size.x / 2,size.y / 2);
-    player.setPosition(320,240);
-
-    // Game loop
-    while (window.isOpen()) {
-        Event event;
-        while(window.pollEvent(event)) {
-            if (event.type == Event::Closed)
-                // close the window
-                window.close();
-        }
-
-        window.clear(Color::Black);
-        // Draw here
-        window.draw(player);
-        window.display();
-
+    while (!game.GetWindow() ->IsDone()) {
+        //Game loop
+        game.HandleInput();
+        game.Update();
+        game.Render();
+        game.RestartClock();
     }
 
     return 0;
